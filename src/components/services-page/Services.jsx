cdc3fe22885da63daf80f22ddcb6e1d2e3876846
Lape15 <larape1993@gmail.com>
2020-09-19 15:28:00 +0100

    services page ready

--- src/components/services-page/Services.jsx
+++ src/components/services-page/Services.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./services.css";
 import Footer from "../about/Footer";
+import software from "../../assets/sub_Images/software.svg";
+import network from "../../assets/sub_Images/network.svg";
 const Services = () => {
   const [load, setLoad] = useState(false);
 
@@ -30,9 +32,40 @@ const Services = () => {
             extensive knowledge and experience in the following areas.
           </div>
         </div>
-        <div className="services">
+        <div className="services-main">
           Section three about to bang yo
-          <div className="services-container">Service items</div>
+          <div className="services-container">
+            <div className="first_service">
+              <div className="inner_first_software">
+                <img src={software} alt="software" className="img" />
+                <h3 className="title">Enterprise software development </h3>
+              </div>
+              <div className="inner_second">
+                <ul className="list">
+                  <li>Website design, development and maintenance</li>
+                  <li>Mobile and desktop appliction development</li>
+                  <li>Software infrastructure management</li>
+                  <li>Software development traning and consultancy</li>
+                </ul>
+              </div>
+            </div>
+            <div className="second_service">
+              <div className="inner_first_networking">
+                <img src={network} alt="network" className="img" />
+                <h3 className="title">Networking and cabling</h3>
+              </div>
+              <div className="inner_second">
+                {" "}
+                <ul className="list">
+                  <li>LAN, WAN, WLAN, PAN, etc.</li>
+                  <li>Computer networks</li>
+                  <li>Structured cabling</li>
+                  <li>Distributed Antenna Systems (DAS)</li>
+                  <li>Network security systems</li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
